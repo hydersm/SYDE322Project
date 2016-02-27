1cc693adc9f8612dcef2994db2a56343f4c3c225
Hyder SM <hydersmh@gmail.com>
2016-02-26 22:59:28 -0500

    implemented multiplayer with photon

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class NetworkManager : Photon.PunBehaviour {
+
+	public string gameVersion = "1.0";
+
+	// Use this for initialization
+	void Start () {
+		//PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings (this.gameVersion);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public override void OnJoinedLobby ()
+	{
+		base.OnJoinedLobby ();
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	public void OnPhotonRandomJoinFailed() {
+		Debug.Log ("Random room join failed! Creating a new room.");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	public override void OnJoinedRoom() {
+		PhotonNetwork.Instantiate ("SCPlayer", new Vector3(0f, 0.5f, 0f), Quaternion.identity, 0);
+	}
+}
---
+++ Assets/Scripts/SCNetworkCharacter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class SCNetworkCharacter : Photon.PunBehaviour {
+
+	public Material playerMaterial;
+	private Vector3 correctPlayerPos;
+
+	// Use this for initialization
+	void Start () {
+		if (this.photonView.isMine) {
+			this.GetComponent<PlayerController> ().enabled = true;
+			this.GetComponent<Renderer>().material = playerMaterial;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!this.photonView.isMine) {
+			this.transform.position = Vector3.Lerp (this.transform.position, correctPlayerPos, Time.deltaTime * 5);
+		}
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			stream.SendNext (transform.position);
+		} else {
+			this.correctPlayerPos = (Vector3)stream.ReceiveNext ();
+		}
+	}
+}
