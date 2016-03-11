9c9907ee597d6cf13c82718e59186956afb6870d
Hyder SM <hydersmh@gmail.com>
2016-03-11 00:22:56 -0500

    changed the environment

---
+++ Assets/Scripts/Car/SCCarCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCCarCollision : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.centerOfMass = Vector3.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
